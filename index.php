4709f1d67d53f877eb1cac84f45d438dda162979
B6122553 <B6122553@g.sut.ac.th>
2021-01-15 13:51:10 +0700

    day3-43

--- index.php
+++ index.php
@@ -62,7 +62,7 @@
                               <div class="col-sm-4 mb-5 bg-white rounded">
                                <div class="card" >
                                <br>
-                                <center><div><img style="border: solid 2px #f26522 ; border-radius: 10px;" class="card-top-img" src="resource/images/upload/<?php echo $auths['image'];?>" width="70%" height="290px" alt="Card image"></div></center>
+                                <center><div><img class="card-top-img" src="resource/images/upload/<?php echo $auths['image'];?>" width="70%" height="290px" alt="Card image"></div></center>
                                 <div class="card-body">
                                  <p class="card-text" style="margin: 0% 5% 0% 5%; font-size: 18px;" >            
                                   <?php echo $auths['FirstName']."  ".$auths['LastName'].'<br/>';
