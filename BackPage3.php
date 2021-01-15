24e9d4929a47c31e2f22a09c502812910b61a969
B6122553 <B6122553@g.sut.ac.th>
2021-01-15 10:18:30 +0700

    day3-21

--- BackPage3.php
+++ BackPage3.php
@@ -102,7 +102,7 @@
                               <labe>ปีการศึกษา</labe><br/>
                               <input type="text" name="dates" class="form-control w-50" placeholder="พ.ศ."><br/>
                               <labe>รูปภาพ</labe>
-                              <input type="file" name="file" class="form-control">
+                              <input type="file" name="file" >
                               <labe>ชื่อ-นามสกุล</labe>
                               <input type="text" name="fullname" class="form-control w-100" placeholder="ชื่อ-นามสกุล">
                               <label class="col-form-label">ชื่อปริญญา</label>
