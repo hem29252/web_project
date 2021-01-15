abe64b9053b4c4b71fdfc4f0de7449483612f782
B6122553 <B6122553@g.sut.ac.th>
2021-01-15 11:11:34 +0700

    day3-36

--- page3.php
+++ page3.php
@@ -61,7 +61,7 @@
                                ?>
                               <tr>
                                 <td><?php echo $degree['Dates']; ?></td>
-                                <td><img src="resource/images/upload/<?php echo $degree['FullName'];?>"></td>
+                                <td><img src="resource/images/upload/<?php echo $degree['FullName'];?>" width="100px" height="100px"></td>
                                 <td style="text-align: left;"><?php echo $degree['image']; ?></td>
                                 <td><a href="<?php echo $degree['link']; ?>"><?php echo $degree['DegreeName']; ?></a></td>
                               </tr>
